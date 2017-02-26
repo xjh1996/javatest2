a9ec65367a02f7da196787e20cf472dbf100d70a
xjh1966 <710804429@163.com>
2017-02-26 15:22:59 +0800

    new things about String

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import javax.lang.model.element.Name;
 import java.math.BigDecimal;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.*;
 
 public class Main {
 
@@ -23,13 +22,24 @@ public class Main {
 
     public static void main(String[] args) {
         Person person = new Person();
-        Person person1=new Person();
-        System.out.println(person.equals(person1));
-        Collection collection =new HashSet();
+        Person person1 = new Person();
+        Persons persons = new Persons();
+        //结果不为true，因为object的equal方法的比较为return this==obj;比较的是引用的地址
+        //若要使其为true可重写equal方法，hashcode也同理
+        System.out.println("相同内容对象通过equal()比较相等是否为true" + person.equals(person1));
+        System.out.println("相同内容对象通过hashCode()比较相等是否为true" + (person.hashCode() == person1.hashCode()));
+        //Collection collection =new HashSet();
 
+        //List<String> list=new ArrayList<>();
+        //list.add(233);
+        String str = "2333";
+        person.name = str;
+        str = "1234";
+        //不能改变person.name中的值，name中存储的是"2333"字符串在堆中的地址，
+        //String类型非基本数据类型，但是它与其他类不同的是，它每次赋值时都会
+        // 查找常量池中的是否存在，没有则创建该字符串
+        System.out.println("引用赋值改变能否成功" + person.name + "," + person.id);
         person.name = "2333";
-        person1.name="2333";
-        System.out.println(person.hashCode()==person1.hashCode());
         person.id = 2333;
         System.out.println("原名" + person.name + "," + person.id);
         changePersonName(person);
@@ -46,4 +56,8 @@ class Person {
 
     String name;
     int id;
+}
+
+class Persons {
+    Person person1;
 }
